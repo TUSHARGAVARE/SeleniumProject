d208e3ba4a82a8822a2ff2eecfe1a78e02947d81
Tushar Gavare <Tushar Gavare@TUSHAR>
2024-05-18 10:13:16 +0530

    commit org

--- SeleniumProject/src/test/java/package1/ContactTest.java
+++ SeleniumProject/src/test/java/package1/ContactTest.java
@@ -5,8 +5,9 @@ import org.testng.annotations.Test;
 public class ContactTest {
 	@Test
 	public void createcontactTest() {
+		
 		String URL = System.getProperty("url");
-		String BROWSER = System.getProperty("browser");
+		String BROWSER = System.getProperty("browser","chrome");
 		String USERNAME = System.getProperty("username");
 		String PASSWORD = System.getProperty("password");
 		System.out.println(URL);
